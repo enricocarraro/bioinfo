cd412ff85a7ce92453c7fc04d0af7cd89a65e34a
Enrico Carraro <enrico.carraro@studenti.polito.it>
2021-05-04 13:08:59 +0200

    Edit documentation of random_fastaq_generator.

--- random_fastaq_generator/rand_fasta_generator.py
+++ random_fastaq_generator/rand_fasta_generator.py
@@ -1,13 +1,15 @@
-# Assignment:
-#
-# Write a python program that generates both fasta and fastq files containing reads with the following characteristics:
-# -Read id contains a progressive number starting from 0. -Sequences have length 50 bp
-# -Bases are randomly generated using a A,T,C,G alphabet, but probability of each base for each read should be given from the command line as a set of numbers (probA, probT, probC, probG)
-# -The number of reads should be passed as an argument from the command line
-# -The name of the fasta/fastq file should be passed as an argument from the command line
-# -For fastq files only: the quality of each base is randomly selected.
-# Example:
-# python3 program.py simulatedfasta.fa 100 30 30 30 10
+"""
+Assignment: Random fasta and fastq file generator
+
+Write a python program that generates both fasta and fastq files containing reads with the following characteristics:
+-Read id contains a progressive number starting from 0. -Sequences have length 50 bp
+-Bases are randomly generated using a A,T,C,G alphabet, but probability of each base for each read should be given from the command line as a set of numbers (probA, probT, probC, probG)
+-The number of reads should be passed as an argument from the command line
+-The name of the fasta/fastq file should be passed as an argument from the command line
+-For fastq files only: the quality of each base is randomly selected.
+Example:
+python3 program.py simulatedfasta.fa 100 30 30 30 10
+"""
 
 
 import random
