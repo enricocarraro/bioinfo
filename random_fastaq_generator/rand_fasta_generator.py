69cb812ead9339f3f5cc8c3ba38df14c34f548e5
Enrico Carraro <enrico.carraro@studenti.polito.it>
2021-05-04 13:00:28 +0200

    Move program to folder and format.

--- random_fastaq_generator/rand_fasta_generator.py
+++ random_fastaq_generator/rand_fasta_generator.py
@@ -7,7 +7,7 @@
 # -The name of the fasta/fastq file should be passed as an argument from the command line
 # -For fastq files only: the quality of each base is randomly selected.
 # Example:
-# python3 simulatedfasta.fa 100 30 30 30 10
+# python3 program.py simulatedfasta.fa 100 30 30 30 10
 
 
 import random
@@ -15,14 +15,14 @@ import sys
 from itertools import accumulate
 
 if len(sys.argv) != 7:
-    raise Exception("Program needs 7 parameters.");
+    raise Exception("Program needs 7 parameters.")
 
 _, output_file, sequences, *probs = sys.argv
 
-probs = list(map(int, probs));
-prefix_probs = list(accumulate(probs));
+probs = list(map(int, probs))
+prefix_probs = list(accumulate(probs))
 
-print(output_file, sequences, probs);
+print(output_file, sequences, probs)
 
 fa = output_file.endswith(".fa")
 
@@ -31,25 +31,26 @@ with open(output_file, 'w') as fast:
     sys.stdout = fast
 
     for sequence in range(0, int(sequences)):
-        print(">" if fa else "@", sequence, sep = '')
+        print(">" if fa else "@", sequence, sep='')
         t, a, c, g = [0, 0, 0, 0]
         for char in range(50):
             toss = random.randint(1, 101)
             if toss <= prefix_probs[0]:
-                print("T", end = "")
+                print("T", end="")
                 t += 1
             elif toss <= prefix_probs[1]:
-                print("A", end = "")
+                print("A", end="")
                 a += 1
             elif toss <= prefix_probs[2]:
-                print("C", end = "")
+                print("C", end="")
                 c += 1
             else:
-                print("G", end = "")
+                print("G", end="")
                 g += 1
         print("")
         if not fa:
-            print("@", sequence, sep = '')
-            print(''.join(list(map(chr, [random.randint(33, 73) for _ in range(50)]))))
+            print("@", sequence, sep='')
+            print(
+                ''.join(list(map(chr, [random.randint(33, 73) for _ in range(50)]))))
 
     sys.stdout = original_stdout
